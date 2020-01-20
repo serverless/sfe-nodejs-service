79eb88874db9e73d691d434d8cc6dbaf85517739
Chris McLeod <chris.mcleod@serverless.com>
2020-01-20 13:26:44 -0600

    Add CORS support

--- handler.js
+++ handler.js
@@ -1,16 +1,20 @@
-'use strict';
+"use strict";
 
 module.exports.hello = async event => {
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+    },
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: "Go Serverless v1.0! Your function executed successfully!",
+        input: event
       },
       null,
       2
-    ),
+    )
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
